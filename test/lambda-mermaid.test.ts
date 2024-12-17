110b45eeb308924f45bcf1941fe31069bbb4c90d
martzcodes <978362+martzcodes@users.noreply.github.com>
2024-12-16 20:48:32 -0500

    Initial commit

---
+++ test/lambda-mermaid.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaMermaid from '../lib/lambda-mermaid-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-mermaid-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaMermaid.LambdaMermaidStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
